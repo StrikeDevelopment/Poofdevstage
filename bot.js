2402e96a72352f1646737b8d4f04c8ced31f4436
MrPooofles <38237033+MrPooofles@users.noreply.github.com>
2018-05-20 14:10:54 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const bot = new Discord.Client({disableEveryone: true});
 bot.on("ready", async () => {
  console.log(`${bot.user.username} is online!`);
 
- bot.user.setActivity("Development of Poof Bot", {type: "PLAYING"});
+ bot.user.setActivity("Development of Poof Bot", {type: "WATCHING"});
 
 });
 
